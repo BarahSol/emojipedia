95c69454117ae386816ebb41ee096787490c36a4
BarahSol <nwosujide@gmail.com>
2024-12-17 04:07:13 +0100

    Add files via upload

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: " 😍",
+    name: "Smiling Face With Heart-Eyes",
+    meaning:
+      "A smile of love is shown by the 😍 emoji, which beams with happiness with its heart-shaped eyes. It conveys intense love and affection for a person, place or even an object. This emoji is often used in romantic messages to express love, gratitude, and admiration. It can also be used to convey enthusiasm for a cause or event. The heart-shaped eyes symbolize overwhelming feelings of happiness and a strong connection.",
+  },
+  {
+    id: 5,
+    emoji: "😋",
+    name: "Face Savouring Delicious Food",
+    meaning:
+      "Bon appetit! Licking your lips with pleasure, the 😋 emoji shows an expression of satisfaction, hunger or joy over delicious food. It is shown beaming with joy when you have cooked something particularly tasty or when your plate is empty. It is also often used in conjunction with a humorous message or when joking, as it shows a playful grimace with the tongue sticking out. Similarly, when flirting, it is used for a thoughtful and ambiguous digression. The 😋 emoji is perfect for expressing culinary enthusiasm and enjoyment of food, or simply to convey a fun and relaxed mood.",
+  },
+  {
+    id: 6,
+    emoji: "😜",
+    name: "Face With Stuck-Out Tongue & Winking Eye",
+    meaning:
+      "The 😜 emoji, characterized by a winking eye and stuck out tongue, conveys a cheeky and playful attitude that is ideal for humorous or ironic messages. The wink suggests a certain playfulness or flirtatiousness, making it perfect for situations where people are teasing or charming each other. It conveys that the sender is not serious and that the message is meant with a wink. Therefore, it is ideal for casual and informal communication where lightness and fun are in the foreground. The 😜 emoji can be used in many contexts to add a light and cheerful touch to a conversation and avoid misunderstandings. Also, the combination of tongue and wink shows that the person is open-minded and ready for a joke.",
+  },
+];
+
+export default emojipedia;
